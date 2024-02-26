6dff8f64eef7a09df3c22760109b9a9257dad24e
Justin <thisisjustinmao@gmail.com>
2024-02-25 22:32:24 -0500

    loading spectrogram data in train

--- code/train.py
+++ code/train.py
@@ -1,8 +1,23 @@
 from pathlib import Path
 from os import listdir
+from os.path import join
+import torch
+import numpy as np
+import cnn
+
+torch.manual_seed(0)
+np.random.seed(0)
 
 
-source_path = Path(r"code/raw_data/audio/musicnet/train_data")
-files = listdir(source_path)
+train_path = Path(r"code/raw_data/spectrogram_train")
+test_path = Path(r"code/raw_data/spectrogram_test")
+
+train_files = listdir(train_path)
+test_files = listdir(test_path)
+
+train_loader = torch.utils.data.DataLoader(train_files, batch_size=4, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_files, batch_size=4, shuffle=True)
+# add validation set
+
 
 # 320 train, 10 test
