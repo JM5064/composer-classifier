1a7bd62d41117526dafc1b9aeb9914352c58e428
Justin <thisisjustinmao@gmail.com>
2024-02-25 20:20:26 -0500

    trimmed spectrograms and created new spectrogram_train and test

--- code/data_processing.py
+++ code/data_processing.py
@@ -15,15 +15,19 @@ for filename in files:
     # create melspectrogram
     y, sr = librosa.load(join(source_path, filename))
     S = librosa.feature.melspectrogram(y=y, sr=sr)
-    fig, ax = plt.subplots()
     S_dB = librosa.power_to_db(S, ref=np.max)
+
+    # create image
+    fig, ax = plt.subplots()
     img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
-    fig.colorbar(img, ax=ax, format='%+2.0f dB')
+    # turn off axis and remove padding
+    ax.axis('off')
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
     # save into folder
     filename_parts = filename.split(".")
     output_name = "code/raw_data/spectrogram/" + filename_parts[0] + ".png"
-    
+
     fig.savefig(output_name)
     plt.close()
 
