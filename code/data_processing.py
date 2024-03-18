f8b47dff79c90d10b0c6522f6dd4274bdc789141
Justin <thisisjustinmao@gmail.com>
2024-03-17 21:17:38 -0400

    split_wav_files

--- code/data_processing.py
+++ code/data_processing.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import numpy as np
 from os import listdir
 from os.path import join
+import os
+import soundfile as sf
 
 train_path = Path(r"code/raw_data/audio/musicnet/train_data/")
 test_path = Path(r"code/raw_data/audio/musicnet/test_data/")
@@ -11,10 +13,25 @@ train_files = listdir(train_path)
 test_files = listdir(test_path)
 
 
+# split each wav file into segments and export them to destination_directory
+def split_wav_files(files, source_path, destination_path, split_duration):
+    for file in files:
+        audio_data, sample_rate = sf.read(str(source_path) + "/" + file)
+
+        num_splits = int((len(audio_data) / sample_rate) // split_duration)
+        print(file[0:4])
+
+        for i in range(num_splits):
+            start = sample_rate * split_duration * i
+            end = sample_rate * split_duration * (i+1)
+            split_section = audio_data[start:end]
+
+            output_file = os.path.join(destination_path, f"{file[0:4]}_{i}.wav")
+            sf.write(output_file, split_section, sample_rate)
+
+
 def create_spectrograms(source_path, files):
-
     for filename in files:
-
         print(f"Converting {filename} now")
 
         # create melspectrogram
@@ -38,7 +55,7 @@ def create_spectrograms(source_path, files):
 
     print("Done Converting!")
 
-
+# create_labels() NOT NEEDED
 # 0 - Schubert
 # 1 - Mozart
 # 2 - Dvorak
@@ -52,44 +69,44 @@ def create_spectrograms(source_path, files):
 
 # Create the labels for composers
 def create_labels(files, output_file_name):
-
     f = open("code/raw_data/" + output_file_name, "a")
 
     for filename in files:
         file_number = int(filename[:4])
 
-        composer = 0    # schubert
+        composer = 0  # schubert
 
         if 1788 <= file_number <= 1893:
-            composer = 1    # mozart
+            composer = 1  # mozart
         elif 1916 <= file_number <= 1933:
-            composer = 2    # dvorak
+            composer = 2  # dvorak
         elif 2075 <= file_number <= 2083:
-            composer = 3    # cambini
+            composer = 3  # cambini
         elif 2104 <= file_number <= 2106:
-            composer = 4    # haydn
+            composer = 4  # haydn
         elif 2112 <= file_number <= 2161:
-            composer = 5    # brahms
+            composer = 5  # brahms
         elif 2166 <= file_number <= 2169:
-            composer = 6    # faure
-        elif 2177 <= file_number <= 2180 or 2802 <= file_number <= 2808:    # no ravel in test?
-            composer = 7    # ravel
+            composer = 6  # faure
+        elif 2177 <= file_number <= 2180 or 2802 <= file_number <= 2808:  # no ravel in test?
+            composer = 7  # ravel
         elif 2186 <= file_number <= 2310:
-            composer = 8    # bach
+            composer = 8  # bach
         elif 2313 <= file_number <= 2678:
             # there's one recording by bach with number 2659
             if file_number == 2659:
                 composer = 8
             else:
-                composer = 9    # beethoven
+                composer = 9  # beethoven
 
         f.write(str(composer) + "\n")
         print(filename)
     f.close()
 
 
+split_wav_files(test_files, test_path, "composer-classifier/raw_data/audio/musicnet/split_test_data", 30)
+split_wav_files(test_files, test_path, "composer-classifier/raw_data/audio/musicnet/split_test_data", 30)
 # create_labels(train_files, "train_labels.txt")
 # create_labels(test_files, "test_labels.txt")
 # create_spectrograms(train_path, train_files)
 # create_spectrograms(test_path, test_files)
-
