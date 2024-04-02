4289ce39d063e3339d9b665172e938ef16b53ffc
Justin <thisisjustinmao@gmail.com>
2024-04-02 18:43:14 -0400

    dataset image opening

--- code/dataset.py
+++ code/dataset.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 from os import listdir
+from PIL import Image
 
 
 class MusicDataset(Dataset):
@@ -11,19 +12,27 @@ class MusicDataset(Dataset):
         self.files = listdir(self.path)
 
         # load csv file for labels
-        self.df = pd.read_csv("/Users/justinmao/Documents/GitHub/composer-classifier/raw_data/musicnet_metadata_short.csv")
+        self.df = pd.read_csv("raw_data/musicnet_metadata.csv")
         self.labels = self.df[['composer']]
 
         self.dataset = torch.tensor(self.labels.to_numpy().reshape(-1).long())
+
+        self.transform = torch.transforms.Compose([
+            torch.transforms.ToTensor(),
+            torch.transforms.Normalize((0.5,), (0.5,))
+        ])
 
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, item):
         image_path = self.path + "/" + str(self.files[item])
-        image_num = self.files[item][:4]
 
+        image = Image.open(image_path)
+        image = self.transform(image)
+
+        image_num = self.files[item][:4]
         label = self.df.loc[image_num, 'composer']
 
-        return image_path, label
+        return image, label
 
